86b9a919063b8358bc9e8321325efd29dafbffec
Pms-crypto <poorvashukla315@gmail.com>
2024-03-08 15:16:10 +0530

    feat: add features and faqs component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import {Navbar} from "./components/Navbar";
 import {Hero} from "./components/Hero";
 import {Features} from "./components/Features";
+import { Faq } from "./components/Faq";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <div className="px-[20px] lg:container lg:px-20 mx-auto">
       <Features />
+      <Faq />
       </div>
     </>
   );
